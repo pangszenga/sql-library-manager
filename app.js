f19d1a0e0b25918b6f5fbe4dee90925585996043
Priscilla Pang <41451092+ppdevelopmentinc@users.noreply.github.com>
2019-08-21 11:22:36 +0100

    Removed unused code

--- app.js
+++ app.js
@@ -32,17 +32,6 @@ app.use(function(err, req, res, next) {
   next(createError(500));
 });
 
-// error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render("error");
-// });
-
 app.use((req, res, next) => {
   const error = new Error("PAGE NOT FOUND!!");
   error.status = 404;
